e1e35b2f54dffc7f9af45fdede0081a2e89ff3fb
CodyCBakerPhD (Engaging) <cody.c.baker.phd@gmail.com>
2025-12-14 21:31:39 -0500

    first run

--- script.sh
+++ script.sh
@@ -13,6 +13,7 @@ cd /orcd/data/dandi/001/all-dandi-compute/blobs-to-paths
 python update.py
 
 git add blob_id_to_path.json
+git add blob_id_to_path.min.json
 git commit --message "update" | true
 git push
 
