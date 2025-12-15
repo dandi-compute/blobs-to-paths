8704baca809a1c018f2d840a6cf0fbcc96b96e28
Cody Baker <cody.c.baker.phd@gmail.com>
2025-12-14 21:10:18 -0500

    change to sbatch

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+
+#SBATCH --partition mit_normal
+#SBATCH --output /dev/null
+#SBATCH --cpus-per-task=1
+
+source /etc/profile.d/modules.sh  # When run via crontab, this is needed to load the modules
 module load miniforge
 
-source activate dandi-compute
+conda activate /orcd/data/dandi/001/environments/name-dandi+compute_env
 
 cd /orcd/data/dandi/001/all-dandi-compute/blobs-to-paths
 python update.py
@@ -10,4 +17,4 @@ git commit --message "update" | true
 git push
 
 # CRON
-#
+# 0 6 * * * flock -n /orcd/data/dandi/001/flocks/update_blobs_to_paths.lock sbatch /orcd/data/dandi/001/all-dandi-compute/blobs-to-paths/script.sh
